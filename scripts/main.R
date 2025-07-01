f9a2bf10487c6b01c46d847c2ba217f1ced7f203
Louise Bernard <louise.bernard01@gmail.com>
2025-07-01 17:03:31 +0000

    Change order for running and code fixes

--- scripts/main.R
+++ scripts/main.R
@@ -50,12 +50,14 @@ red_palette <- c("#FF9999", "#FF8080", "#FF6666", "#FF4D4D", "#FF3333", "#FF1A1A
 random_subsample <- FALSE
 
 # Cosy reproduction
+source("scripts/02_00_cosy.R")
+
+
 source("scripts/01_00_heatpump.R")
 rm(list = setdiff(ls(), c("random_subsample", "flexible_color", "cosy_color", "hp_color", 
                           "not_hp_color", "rating_colors", "red_palette")))
 gc()
 
-source("scripts/02_00_cosy.R")
 
 source("scripts/03_00_balance_tables_and_reweighting.R")
 
