f5fa025abe55577483f565e032331644f1c7467e
Yixin Sun <yixinsun1216@gmail.com>
2025-12-05 14:52:44 +0000

    this table formatting will be the death of me

--- scripts/02_07_heterogeneity_analysis.R
+++ scripts/02_07_heterogeneity_analysis.R
@@ -309,7 +309,10 @@ etable(m3, title = "Cosy Adoption by Previous Tariff Type",
        dict = c(previous_is_charged_half_hourly = "Prev is ToU", 
                cosy_contract_active = "Cosy Contract Active", 
                 rate_period = "Rate Period",
-                consumption_hh = "Consumption in kWh"), 
+                consumption_hh = "Consumption in kWh", 
+               account_id = "Household", 
+               hdd = "HDD", 
+               day = "Day"), 
        file = "tables/did_prevar.tex", replace = TRUE)
 
 # Read the generated LaTeX file
