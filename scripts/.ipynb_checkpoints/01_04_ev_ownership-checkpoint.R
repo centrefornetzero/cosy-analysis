e133c68c04564b62a1fa84caab85cecec419c760
Yixin Sun <yixinsun1216@gmail.com>
2025-12-22 16:23:31 +0000

    revert ev analysis back to original formulation

--- scripts/.ipynb_checkpoints/01_04_ev_ownership-checkpoint.R
+++ scripts/.ipynb_checkpoints/01_04_ev_ownership-checkpoint.R
@@ -19,21 +19,36 @@ ev_charging <- fread(file.path(datapath, "input/cosy_-_ev_detection_2024_07_04.c
          )
   )
 
+# Aggregate at the account id, mpan, date and rate period level
+ev_charging_agg <- rbind(ev_charging %>%
+                           group_by(account_id,  hashed_mpan, date, rate_period) %>%
+                           tally(),
+                         ev_charging %>%
+                           group_by(account_id,  hashed_mpan, date) %>%
+                           tally() %>% 
+                           mutate(rate_period="Overall")) %>%
+  rename(ev_charging=n)
+
 # EV users details
 ev_users <- ev_charging %>%
   group_by(account_id) %>%
   summarise(is_ev_detected= min(as.Date(interval_start)))
 
 # Update hp_installed with the new ev_charging values using case_when
-hp_installed <- 
-  read_rds(file.path(datapath, "output/hp_installed.rds")) %>%
+hp_installed <- hp_installed %>%
+  left_join(ev_charging_agg) %>%
+  mutate(ev_charging = ifelse(is.na(ev_charging), 0, ev_charging),
+         ev_charging = case_when(
+           rate_period == "Overall" ~ ev_charging / 48,
+           rate_period == "Other" ~ ev_charging / 30,
+           TRUE ~ ev_charging / 6
+         )
+  ) %>%
   left_join(ev_users) %>%
   mutate(has_ev = as.numeric(is_ev_detected <= date),
          has_ev = ifelse(is.na(has_ev), 0, has_ev))
 
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# --------- Consumption impacts by EV ownership --------------
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Fit the model
 m1 <- feols(total_consumption ~ i(is_hp_installed, ref=0)  |
               hdd + account_id + date, 
             data = hp_installed %>% 
@@ -56,37 +71,41 @@ etable(m1, m1c, tex = TRUE, title = "HP Installation on Electricity Consumption 
        fitstat = ~ N + g + pre_avg + t_obs + r2, 
        file = "tables/hp_did_ev.tex", replace = TRUE, label = "tab:hp-did-ev")
 
-
 CleanPreAverage("tables/hp_did_ev.tex")
 
 
 
 
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-## Table A.4: HP Installation on Probability of Charging EV by Period
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-ev_charging_balanced <- 
-  ev_charging %>%
-  # get all unique account_id-date combinations
-  distinct(account_id, date) %>%
-  # cross with all possible rate_period values
-  crossing(rate_period = unique(ev_charging$rate_period)) 
 
-# For each day-period, identify if charging occured 
-ev_charging_probability <- ev_charging %>%
-  group_by(account_id, date, rate_period) %>%
-  summarise(ev_charging = sum(ev_charging, na.rm = TRUE) > 0) %>%
-  right_join(ev_charging_balanced) %>%
-  mutate(ev_charging = replace_na(ev_charging, 0))%>%
-  pivot_wider(id_cols = c(account_id, date), names_from = "rate_period", values_from = "ev_charging")  %>%
-  left_join(select(hp_installed, account_id, date, is_hp_installed)) %>%
-  rename_all(~str_replace(.x, "\\s", "_")) 
+## Table A.4: HP Installation on Probability of Charging EV by Period
+
+# Identify the period with the highest EV charging for each mpan and date
+ev_charging_max <- ev_charging %>%
+  group_by(account_id, hashed_mpan, date, rate_period) %>%
+  summarise(ev_charging = sum(ev_charging, na.rm = TRUE)) %>%
+  group_by(account_id, hashed_mpan, date) %>%
+  filter(ev_charging == max(ev_charging)) %>%
+  mutate(highest_ev_charging = 1) %>%
+  ungroup()
+
+ev_charging_max <- ev_charging_max %>%
+  left_join(hp_installed %>% select(account_id, hashed_mpan, date, is_hp_installed) %>% distinct())
+
+# Create dummy variables for rate periods
+ev_charging_max <- ev_charging_max %>%
+  mutate(
+    Morning_Cosy = ifelse(rate_period == "Morning Cosy", 1, 0),
+    Afternoon_Cosy = ifelse(rate_period == "Afternoon Cosy", 1, 0),
+    Peak_Rate = ifelse(rate_period == "Peak Rate", 1, 0),
+    Other = ifelse(rate_period == "Other", 1, 0)
+  )
 
 # Run the fixed effects models
-m_charging1 <- feols(Morning_Cosy ~ i(is_hp_installed) | account_id + date, data = ev_charging_probability, cluster = ~ account_id)
-m_charging2 <- feols(Afternoon_Cosy ~ i(is_hp_installed) | account_id + date, data = ev_charging_probability, cluster = ~ account_id)
-m_charging3 <- feols(Peak_Rate ~ i(is_hp_installed) | account_id + date, data = ev_charging_probability, cluster = ~ account_id)
-m_charging4 <- feols(Other ~ i(is_hp_installed) | account_id + date, data = ev_charging_probability, cluster = ~ account_id)
+m_charging1 <- feols(Morning_Cosy ~ i(is_hp_installed) | account_id + date, data = ev_charging_max, cluster = ~ account_id)
+m_charging2 <- feols(Afternoon_Cosy ~ i(is_hp_installed) | account_id + date, data = ev_charging_max, cluster = ~ account_id)
+m_charging3 <- feols(Peak_Rate ~ i(is_hp_installed) | account_id + date, data = ev_charging_max, cluster = ~ account_id)
+m_charging4 <- feols(Other ~ i(is_hp_installed) | account_id + date, data = ev_charging_max, cluster = ~ account_id)
+
 
 # Generate the LaTeX table with the dependent variable named "Charging EV"
 etable(m_charging1, m_charging2, m_charging3, m_charging4, 
@@ -96,7 +115,7 @@ etable(m_charging1, m_charging2, m_charging3, m_charging4,
        fitstat = ~ N + g + pre_avg + r2, 
        file = "tables/hp_ev_charging.tex", 
        replace = TRUE, 
-       label = "tab:hp-ev-charging", 
+       label = "tab:hp-ev-charging",
        dict = c(Morning_Cosy = "Charging EV", 
                 Afternoon_Cosy = "Charging EV", 
                 Peak_Rate = "Charging EV", 
@@ -109,16 +128,15 @@ file_path <- "tables/hp_ev_charging.tex"
 file_content <- readLines(file_path)
 
 # Find the lines with the pre-treatment average and remove them
-if (length(grep("Yearly Consumption", file_content))==1) {
-  pre_avg_line_index <- grep("Yearly Consumption", file_content)
+if (length(grep("Charging EV", file_content))==1) {F
+  pre_avg_line_index <- grep("Charging EV", file_content)
 } else {
-  pre_avg_line_index <- grep("Yearly Consumption", file_content)[2]
+  pre_avg_line_index <- grep("Charging EV", file_content)[2]
 }
 
 # Find the lines with the pre-treatment average and remove them
 pre_avg_lines <- file_content[pre_avg_line_index:(pre_avg_line_index)]
 file_content <- file_content[-c(pre_avg_line_index, pre_avg_line_index)]
-pre_avg_lines <- str_replace(pre_avg_lines, "Yearly Consumption", "Charging EV")
 
 # Find the position just after the coefficients
 coeff_end_index <- grep("Is HP Installed", file_content)[length(grep("Is HP Installed", file_content))] + 2
